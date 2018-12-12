9c5502208222e5a6e8b10a1baea28cf321a5c421
milan_25 <milan.abraham2@gmail.com>
2018-12-12 21:26:17 +0100

    python skript

---
+++ src/test/pythonscript/weatherscript.py
@@ -0,0 +1,26 @@
+#pip install mysql-connector
+#pip install pyowm
+
+import pyowm
+import asyncio
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="139.59.139.93",
+  user="app",
+  passwd="pocasi",
+  database="pocasi"
+)
+
+
+owm = pyowm.OWM("353b379036c2911483cfb9147c1ec9f0")
+
+cities = ["praha", "brno",  "ostrava", "plzen", "ceske budejovice", "hradec kralove", "jihlava", "karlovy vary", "liberec", "olomouc", "pardubice", "usti nad labem", "zlin"]
+
+
+
+for city in cities:
+    weather = owm.weather_at_place(city + ",CZ")
+    w = weather.get_weather()
+    print("{}: {} °C".format(city, str(w.get_temperature('celsius')["temp"])))
+    asyncio.wait(0.5)
